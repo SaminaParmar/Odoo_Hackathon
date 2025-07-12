370c304bc0a5af69a4dee422f1cfaf5d4248b832
savitri <savitrigamit123456@gmail.com>
2025-07-12 10:10:21 +0530

    added express server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static("frontend"));
+
+app.get("/", (req, res) => {
+  res.send("/frontend/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
